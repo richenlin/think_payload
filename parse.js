80810cf475e83133bc0146d736d04c6234da97b0
yanglin <richenlin@gmail.com>
2017-07-20 11:51:14 +0800

    update version

--- parse.js
+++ parse.js
@@ -6,23 +6,11 @@
  * @version    17/5/2
  */
 
-const lib = require('think_lib');
 const form = require('./lib/form.js');
 const json = require('./lib/json.js');
 const multipart = require('./lib/multipart.js');
 const text = require('./lib/text.js');
 const xml = require('./lib/xml.js');
-
-// default json types
-const jsonTypes = ['application/json'];
-// default form types
-const formTypes = ['application/x-www-form-urlencoded'];
-// default text types
-const textTypes = ['text/plain'];
-// default multipart-form types
-const multipartTypes = ['multipart/form-data'];
-// default xml types
-const xmlTypes = ['text/xml'];
 
 /**
  * 
@@ -37,28 +25,20 @@ module.exports = function (ctx, options) {
     if (methods.every(method => ctx.method !== method)) {
         return Promise.resolve({});
     }
-    let extTypes = {
-        json: jsonTypes,
-        form: formTypes,
-        text: textTypes,
-        multipart: multipartTypes,
-        xml: xmlTypes
-    };
-    options.extTypes && (extTypes = lib.extend(extTypes, options.extTypes));
 
-    if (ctx.request.is(extTypes.json)) {
+    if (ctx.request.is(options.extTypes.json)) {
         return json(ctx, options);
     }
-    if (ctx.request.is(extTypes.form)) {
+    if (ctx.request.is(options.extTypes.form)) {
         return form(ctx, options);
     }
-    if (ctx.request.is(extTypes.text)) {
+    if (ctx.request.is(options.extTypes.text)) {
         return text(ctx, options);
     }
-    if (ctx.request.is(extTypes.multipart)) {
+    if (ctx.request.is(options.extTypes.multipart)) {
         return multipart(ctx, options);
     }
-    if (ctx.request.is(extTypes.xml)) {
+    if (ctx.request.is(options.extTypes.xml)) {
         return xml(ctx, options);
     }
 
